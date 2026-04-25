4ae431f1e3a2e480d96d00a16b885e396c9df483
Dilkhush Kumar <dk5847001@gmail.com>
2026-04-25 18:21:09 +0530

    middleware update

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,14 +1,14 @@
-module.exports.isLoggedIn = (req, res, next)=>{
-    if(!req.isAuthenticated()){
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
         req.session.redirectUrl = req.originalUrl; // Store the original URL for redirect after login
         req.flash("error", "You must be logged in to do that!");
-       return res.redirect("/login");
+        return res.redirect("/login");
     }
     next();
 }
 
-module.exports.saveRedirectUrl = (req, res, next)=>{
-    if(req.session.redirectUrl){
+module.exports.saveRedirectUrl = (req, res, next) => {
+    if (req.session.redirectUrl) {
         res.locals.redirectUrl = req.session.redirectUrl; // Make redirect URL available in views
     }
     next();
